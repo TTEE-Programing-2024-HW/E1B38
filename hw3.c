933e4758b702471a901d2be17ad8b58e209dffd2
E1B38 <u11202238@o365.ttu.edu.tw>
2024-05-13 03:07:16 +0800

    最後一次我把C選項寫完了並且修正了一些前面的小錯誤 C選項主要是先讓你手動輸入後，擷取你輸入了數字，讓電腦知道他是行列，然後檢測是否在範圍內或是否已有人，最後把空位變成*就可以了
    心得:我認為這次蠻困難的因為如過靠我自己的力量是寫不出來的，我有上網查資料像是sscanf這個是取的兩個正整數值分別存到row和col指向的內存位置中，如果不查資料完全不知道怎麼寫，但並不是盲目的抄寫，而是要去理解並且吸收成自己的東西，希望自己可以越來越進步!!!

--- hw3.c
+++ hw3.c
@@ -4,6 +4,8 @@ void displayseat();
 void setseat();
 void log1seat();
 void log2seat();
+int parse_seat_choice(const char* seat_choice, int* row, int* col);
+int validate_seat_selection(int row, int col);
 char seat[9][9];
 int main(void){
 	setseat();
@@ -73,7 +75,7 @@ int main(void){
         			getchar();  //消耗換行符
         			displayseat();
         			if(s>=1 && s<= 3){
-        				int start_row = rand()%9; // 隨機行 
+        				int start_row = rand()%9; // 隨機行(0-7)
                     	int start_col = rand()%(9-s+1); //隨機列 
                    		for(i=0;i<s;i++) {
                    		if(seat[start_row][start_col + i] !='*'){
@@ -85,8 +87,8 @@ int main(void){
     					printf("請選擇一種前後兩列各兩個座位(1)/連續4個(2):");
     					scanf("%d",&o);
     					if(o==1){
-               			int start_row = rand() % (9-1);
-               			int start_col = rand() % (9-1) ;
+               			int start_row = rand() % 7;//隨機行(0-6)
+               			int start_col = rand() % 8;//隨機列(0-7) 
                			for (i=start_row;i<start_row+2;i++) {
                     	for (j=start_col;j<start_col+2;j++){
                         seat[i][j] = '@';
@@ -94,8 +96,8 @@ int main(void){
 						}
 					}
     					if(o==2){
-               			int start_row = rand() % (9 - 1 + 1) + 1; //隨機行(1-8) 
-                		int start_col = rand() % (9 - 3 + 1); //隨機列(1-5) 
+               			int start_row = rand() % 8; //隨機行(0-7) 
+                		int start_col = rand() % 6; //隨機列(0-5) 
                 		for (i=0;i<4;i++) {
                 		if(seat[start_row][start_col + i] !='*'){
                     	seat[start_row][start_col + i] = '@'; //連續4個@ 
@@ -122,7 +124,22 @@ int main(void){
     							continue;// 回到循還開始
 							}
                 case 'c':
-                    
+                    printf("請逐一輸入座位選擇，如1-2、2-9代表[列1,行2]及[列2,行9]兩個座位。\n");
+                    printf("手動選擇座位:");
+                    char seat_choice[5];
+                    scanf("%s", seat_choice);
+                    getchar();
+                    int row, col,chosen_seats = 0;
+                    if (parse_seat_choice(seat_choice, &row, &col)) {
+                    	if (validate_seat_selection(row, col)) {
+            				seat[row-1][col-1] = '@';
+            				chosen_seats++;
+       						}
+    				}
+    				printf("更新后的座位表：\n");
+    				displayseat();
+    				log1seat();
+                	break;
                 case 'd':
                 	printf("Continue? (y/n): "); //是否繼續 
                     char k;
@@ -133,7 +150,7 @@ int main(void){
                         } else if (k=='n' || k=='N') {
                            con=0;         // 結束程式
                         } else {
-                            while(k!='n' || k!='y'){
+                            while(k!='n' && k!='y'){
 							printf("錯誤：請輸入 'y' 或 'n'\n");
 							scanf("%c",&k);
                 			getchar();
@@ -154,7 +171,7 @@ void displayseat(){
 	int i,j;
 	printf("\\123456789\n");    //頂部的座位號碼
     for (i=0;i<9;i++){
-        printf("%d",9-i);       //打印座位表的行號
+        printf("%d",i+1);       //打印座位表的行號
         for (j=0;j<9;j++){
             printf("%c",seat[i][j]); //打印座位狀態
         }
@@ -201,5 +218,28 @@ void log2seat(){
         	}
    		}
 	}
+}
+
+int parse_seat_choice(const char* seat_choice, int* row, int* col) {
+    // 如果座位選擇的格式不正确，返回 0
+    if (sscanf(seat_choice, "%d-%d", row, col) != 2) {         //sscanf函數將毒取的兩個正整數值分別存到row和col指向的內存位置中
+        printf("錯誤：請按照格式輸入座位選擇，如1-2、2-9。\n");
+        return 0;
+    }
+    // 如果座位選擇超出範圍，返回 0
+    if (*row < 1 || *row > 9 || *col < 1 || *col > 9) {
+        printf("錯誤：座位選擇超出範圍。\n");
+        return 0;
+    }
+    return 1;    // 座位選擇成功
+    
+}
+
+	 int validate_seat_selection(int row, int col) {
+	 	if (seat[row-1][col-1] != '-') {
+        printf("錯誤：座位已經被選擇。\n");
+        return 0;
+    	}
+    	return 1; // 座位選擇有效
 }
 
