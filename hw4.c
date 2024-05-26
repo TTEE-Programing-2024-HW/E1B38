69f64338f5857ffd1c2d0e2e9ceb30a34c361c58
E1B38 <u11202238@o365.ttu.edu.tw>
2024-05-26 22:39:40 +0800

    第二次上傳，把a題做完，做了一個結構有題目要輸入的選項，把結構帶入函式中，就可以完成。

--- hw4.c
+++ hw4.c
@@ -1,7 +1,62 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
+#include <string.h>
+#define max_st 10
+#define min_st 5
+#define max_name 50
+#define max_id 6
+
+struct st{
+    char name[max_name];
+    int id;
+    int math;
+    int physics;
+    int english;
+};
+
+void inputst(int n,struct st students[]) {
+	int i;
+    for(i=0;i<n;++i) {
+        printf("輸入第%d位學生的資料:\n", i + 1);
+        // 輸入學生姓名
+        printf("姓名: ");
+        while (scanf("%s",students[i].name)!=1) {
+            printf("輸入錯誤，請重新輸入姓名: ");
+            while (getchar() != '\n');  // 清除緩衝區
+        }
+        // 輸入學號
+        printf("學號(6位整數): ");
+        while (scanf("%d", &students[i].id)!=1 || students[i].id<100000 || students[i].id>999999) {
+            printf("輸入錯誤，請重新輸入學號 (6位整數): ");
+            while (getchar() != '\n');  // 清除緩衝區
+        }
+        // 輸入數學成績
+        printf("數學成績 (0~100): ");
+        while (scanf("%d", &students[i].math) != 1 || students[i].math < 0 || students[i].math > 100) {
+            printf("輸入錯誤，請重新輸入數學成績 (0~100): ");
+            while (getchar() != '\n');  // 清除緩衝區
+        }
+        // 輸入物理成績
+        printf("物理成績 (0~100): ");
+        while (scanf("%d", &students[i].physics) != 1 || students[i].physics < 0 || students[i].physics > 100) {
+            printf("輸入錯誤，請重新輸入物理成績 (0~100): ");
+            while (getchar() != '\n');  // 清除緩衝區
+        }
+        // 輸入英文成績
+        printf("英文成績 (0~100): ");
+        while (scanf("%d", &students[i].english) != 1 || students[i].english < 0 || students[i].english > 100) {
+            printf("輸入錯誤，請重新輸入英文成績 (0~100): ");
+            while (getchar() != '\n');  // 清除緩衝區
+        }
+        printf("\n");
+    }
+}
+	
 int main(void){
+	
+	struct st students[max_st];
+    int stcount = 0;
+    
 	printf("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n");   //個人風格畫面 
 	printf("||名子:李維濬               ||\n");
 	printf("------------------------------\n");
@@ -60,7 +115,14 @@ int main(void){
             switch (choice) {
                 case 'a':
                 	system("cls");
-        	      	
+        	      	printf("請輸入學生人數 (5~10): ");
+        	      	while (scanf("%d",&stcount)!=1 || stcount < min_st||stcount > max_st) {
+                    printf("輸入錯誤，請重新輸入學生人數 (5~10): ");
+                    while (getchar() != '\n');
+                }
+                inputst(stcount,students);
+                break;
+                
                 case 'b':
                 	system("cls");
                 
